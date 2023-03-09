af888bbe62bf0422c25956f1077f3d07bbfd3024
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-09 07:02:30 -0800

    added starter code from replit

---
+++ src/components/RequestFormButton.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+
+export default function RequestFormButton() {
+    return (
+        <div>
+            <button>
+                Click Here!
+            </button>
+            <input type="submit" value="Submit" />
+
+        </div>
+    );
+}
