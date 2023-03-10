b01694d92e334fc698f8184b70a9070dc908e1a7
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-09 22:04:51 -0800

    using context provider in RequestFormButton.js and CollectorButton.js

--- src/components/RequestFormButton.js
+++ src/components/RequestFormButton.js
@@ -1,12 +1,15 @@
 import React, { useContext } from "react";
+import StylesContext from './StylesContext';
 
 export default function RequestFormButton() {
+    const buttonStyling = React.useContext(StylesContext)
+
     return (
         <div>
-            <button>
+            <button style={buttonStyling}>
                 Click Here!
             </button>
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Submit" style={buttonStyling} />
 
         </div>
     );
