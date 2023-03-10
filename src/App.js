67cf694080ed19340dd540f3d1d29ecfbfeadd69
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-09 22:02:19 -0800

    adding context provider to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import UserInterface from "./components/UserInterface"
+import StylesContext from './components/StylesContext';
 
 function App() {
   const styling = {
@@ -17,16 +18,18 @@ function App() {
 
   return (
     <main>
-      <h1
-        style={{
-          color: "#FEE001",
-          fontFamily: "Luminari, fantasy",
-          fontSize: 50
-        }}
-      >
-        Welcome to Comics Galore!
-      </h1>
-      <UserInterface />
+      <StylesContext.Provider value={styling}>
+        <h1
+          style={{
+            color: "#FEE001",
+            fontFamily: "Luminari, fantasy",
+            fontSize: 50
+          }}
+        >
+          Welcome to Comics Galore!
+        </h1>
+        <UserInterface />
+      </StylesContext.Provider>
     </main>
   );
 }
